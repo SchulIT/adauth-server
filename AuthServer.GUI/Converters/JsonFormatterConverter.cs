718bbd9752d25a7934c9432491830a3ff8ce3e61
Marcel Marnitz <marcelmarnitz@googlemail.com>
2020-06-29 22:07:52 +0200

    Parameter may be null -> fail fast

--- AuthServer.GUI/Converters/JsonFormatterConverter.cs
+++ AuthServer.GUI/Converters/JsonFormatterConverter.cs
@@ -9,6 +9,11 @@ namespace AuthServer.GUI.Converter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            if(value == null)
+            {
+                return string.Empty;
+            }
+
             try
             {
                 var obj = JsonConvert.DeserializeObject(value.ToString());
